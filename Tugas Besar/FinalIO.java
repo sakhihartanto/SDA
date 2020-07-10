4e6f02f5c9b8b7f2f9407becaf5c5ca488300275
Muhammad Sakhi Hartanto <productionsakhi@gmail.com>
2020-07-10 07:46:06 +0700

    Done But Not Clean Code

--- Tugas Besar/FinalIO.java
+++ Tugas Besar/FinalIO.java
@@ -4,34 +4,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.TreeMap;
 import java.util.Scanner;
+import java.lang.String;
 
 public class FinalIO {
 
     public static void main(String[] args) throws FileNotFoundException {
-        try {
-            File file = new File("text.txt");
-            Scanner sc = new Scanner(file){
-                while (sc.hasNextLine()) {
-                    String data = sc.nextLine();
-                    System.out.println(data);
+
+        TreeMap<String, Integer> map = new TreeMap<String, Integer>();
+
+        try (Scanner txtfile = new Scanner(new File("C:\\Users\\User\\Documents\\NetBeansProjects\\JavaApplication2\\src\\folder\\text.txt"))) {
+            while (txtfile.hasNext()) {
+                String word = txtfile.next();
+                String[] splitkata = word.toLowerCase().split("\\s+|,\\s*|\\.\\s*");
+                int i;
+                for (i = 0; i < splitkata.length; i++) {
+                    Integer count = map.get(splitkata[i]); 
+                    if (map.containsKey(splitkata[i])) {
+                        map.put(splitkata[i], ++count);
+                    } else {
+                        map.put(splitkata[i], 1);
+                    }
+                    
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.println("File Error !");
+            System.out.println(e + " \n " + " FILE NOT FOUND !");
         }
 
-        TreeMap<String, Integer> map = new TreeMap<String, Integer>();
-        String key = word1.toLowerCase();
-        if (key.length() > 0) {
-            if (!map.containsKey(key)) {
-                map.put(key, 1);
-            } else {
-                int value = map.get(key);
-                value++;
-                map.put(key, value);
-            }
-        }
-
-        System.out.println(key + "\t" + value);
+        map.entrySet().stream().forEach((entry) -> {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        });
     }
 }
