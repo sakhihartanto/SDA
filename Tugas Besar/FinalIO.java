e92c4a8998a640bef5941ff3062c64a58fa68365
Muhammad Sakhi Hartanto <productionsakhi@gmail.com>
2020-07-15 15:16:57 +0700

    Add Features + remove operation in text

--- Tugas Besar/FinalIO.java
+++ Tugas Besar/FinalIO.java
@@ -1,4 +1,5 @@
-package folder;
+package TUBES;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,28 +12,33 @@ public class FinalIO {
     public static void main(String[] args) throws FileNotFoundException {
 
         TreeMap<String, Integer> map = new TreeMap<String, Integer>();
-
-        try (Scanner txtfile = new Scanner(new File("C:\\Users\\User\\Documents\\NetBeansProjects\\JavaApplication2\\src\\folder\\text.txt"))) {
-            while (txtfile.hasNext()) {
+        int total=0;
+        try (Scanner txtfile = new Scanner(new File("D:\\Indonesia Negaraku, Polban Kampusku\\Java\\TUBES\\src\\text.txt"))) { 
+            while (txtfile.hasNext()) {  
                 String word = txtfile.next();
-                String[] splitkata = word.toLowerCase().split("\\s+|,\\s*|\\.\\s*");
+                String[] splitkata = word.toLowerCase().split("\\t|,|;|\\.|\\?|!|-|:|@|\\[|\\]|\\(|\\)|\\{|\\}|_|\\*|/\""); 
                 int i;
                 for (i = 0; i < splitkata.length; i++) {
-                    Integer count = map.get(splitkata[i]); 
-                    if (map.containsKey(splitkata[i])) {
-                        map.put(splitkata[i], ++count);
-                    } else {
-                        map.put(splitkata[i], 1);
+                    if(splitkata[i].equals("")){
+                            
+                    }else{                        
+                        Integer count = map.get(splitkata[i]); 
+                        if (map.containsKey(splitkata[i])) {
+                                map.put(splitkata[i], ++count);
+                        } else { 
+                            map.put(splitkata[i], 1);
+                        }
+                        total++;
                     }
-                    
                 }
             }
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {  
             System.out.println(e + " \n " + " FILE NOT FOUND !");
         }
 
         map.entrySet().stream().forEach((entry) -> {
             System.out.println(entry.getKey()+" "+entry.getValue());
         });
+        System.out.println("Total : "+total);
     }
 }
